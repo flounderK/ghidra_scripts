e97f24e1034849956364454fe9c95efd4d178657
Clif Wolfe <clifwolfe1@gmail.com>
2022-04-24 14:55:45 -0400

    Do more analysis, only renamespace if a function is not an external thunk, added comments for functions

--- class_mapper.py
+++ class_mapper.py
@@ -11,6 +11,8 @@ from ghidra.app.decompiler import DecompInterface
 from ghidra.util.task import ConsoleTaskMonitor
 from ghidra.program.flatapi import FlatProgramAPI
 
+# TODO: Try to autofill class structrues based on thisptr
+
 
 class ClassMapper:
     def __init__(self, currentProgram):
@@ -28,9 +30,13 @@ class ClassMapper:
         elif self.ptr_size == 8:
             self._get_ptr_size = self.mem.getLong
 
+        self._null = self.addr_space.getAddress(0)
+        self._global_ns = currentProgram.getGlobalNamespace()
+
         self._thiscall_str = u'__thiscall'
         self._vftable_str = u'vftable'
         self._vtable_str = u'vtable'
+        self._pure_virtual_str = u'pure_virtual'
 
         self._decomp_options = DecompileOptions()
         self._monitor = ConsoleTaskMonitor()
@@ -40,25 +46,42 @@ class ClassMapper:
         # lsm = high_func.getLocalSymbolMap()
         # symbols = lsm.getSymbols()
         self.func_associations = defaultdict(set)
-        self.multiple_inheritance = defaultdict(set)
+        self.multiple_inheritance_functions = defaultdict(set)
+        # store all found vtables/vftables, regardless of duplicates
         self.vftable_entries = {}
+        # store all functions associated with each namespace,
+        # removing duplicates
+        self.namespace_functions = defaultdict(set)
 
         self.class_syms = defaultdict(list)
-        self.get_associated_symbols()
+        self._populate_namespace_associated_symbols()
+        self.analyze_function_associations()
 
     def get_high_function(self, func, timeout=60):
+        """
+        Get a HighFunction for a given function
+        """
         self._ifc.openProgram(func.getProgram())
         res = self._ifc.decompileFunction(func, timeout, self._monitor)
         high_func = res.getHighFunction()
         return high_func
 
-    def get_associated_symbols(self):
+    def _populate_namespace_associated_symbols(self):
+        """
+        Populate a defaultdict(list) with symbols associated
+        with each Class namespace that is currently accessible
+        """
         for namespace in self.sym_tab.getClassNamespaces():
             for s in self.sym_tab.getChildren(namespace.getSymbol()):
                 self.class_syms[s.getParentSymbol().getName()].append(s)
 
-    def associate_vftable_functions_with_namespace(self):
-        null_addr = self.addr_space.getAddress(0)
+    def analyze_function_associations(self):
+        """
+        Iterate each available class namespace to find labels/symbols
+        called vtable/vftable,  and collect data on which
+        class namespace each function can be associated with.
+        """
+        # clean up collected vftable entries to remove duplicates
         for namespace in self.sym_tab.getClassNamespaces():
             for s in self.sym_tab.getChildren(namespace.getSymbol()):
                 usable_vtable_symbol = False
@@ -72,23 +95,95 @@ class ClassMapper:
                     continue
 
                 vftable_entries = self.get_vftable_entries(s)
+                parent_namespace = s.getParentNamespace()
                 self.vftable_entries[s] = vftable_entries
+                # if multiple vtable/vftable symbols are found for the
+                # same namespace, the association should still be made
+                # in the odd occurrance that there are multiple separate
+                # vftables for the same class
+                for e in vftable_entries:
+                    self.namespace_functions[parent_namespace].add(e)
+
                 for func in vftable_entries:
-                    if func == null_addr:
+                    if func == self._null:
                         continue
 
                     self.func_associations[func].add(namespace)
 
+        # Find vtable functions that could fit into multiple namespaces
+        # (this likely means they are inherited)
         for func, namespaces in self.func_associations.items():
             if len(namespaces) == 1:
-                func.setParentNamespace(list(namespaces)[0])
-            else:
-                for n in namespaces:
-                    self.multiple_inheritance[func].add(n)
+                continue
+            for n in namespaces:
+                self.multiple_inheritance_functions[func].add(n)
 
-            func.setCallingConvention(self._thiscall_str)
+    def set_function_associations(self, skip_thiscall_association=False):
+        """
+        Associate functions that are only ever in a single
+        class namespace with that namespace, as non-virtual functions.
+
+        The calling convention for functions is also set to `__thiscall`
+        unless disabled.
+        """
+        # do the namespace association with each func
+        for func, namespaces in self.func_associations.items():
+            if len(namespaces) == 1:
+                if not self.is_external(func):
+                    self.set_parent_namespace_maybe_thunk(func, list(namespaces)[0])
+            else:
+                # TODO: try to identify longer inheritance
+                # structures to pick the
+                # base class to associate the function with
+                virtual_namespaces = [n for n in namespaces if self._is_class_namespace_virtual(n)]
+                # pick off easier associations that are only inherited by
+                # non-virtual classes
+                if len(virtual_namespaces) == 1:
+                    if not self.is_external(func):
+                        self.set_parent_namespace_maybe_thunk(func, list(virtual_namespaces)[0])
+
+            if not skip_thiscall_association:
+                self.set_calling_convention_maybe_thunk(func, self._thiscall_str)
+
+        # search for and associate private functions with each class namespace
+        for namespace in self.namespace_functions.keys():
+            priv_funcs = self._find_private_function_of_class_namespace(namespace)
+            for func in priv_funcs:
+                if not self.is_external(func):
+                    self.set_parent_namespace_maybe_thunk(func, namespace)
+
+                if not skip_thiscall_association:
+                    self.set_calling_convention_maybe_thunk(func, self._thiscall_str)
+
+    def is_external(self, func):
+        if func == self._null:
+            return False
+        dethunked = func
+        if func.thunk:
+            dethunked = func.getThunkedFunction(True)
+
+        return dethunked.external
+
+    def set_calling_convention_maybe_thunk(self, func, calling_convention):
+        while func.thunk:
+            func.setCallingConvention(calling_convention)
+            func = func.getThunkedFunction(False)
+        func.setCallingConvention(calling_convention)
+
+    def set_parent_namespace_maybe_thunk(self, func, namespace):
+        while func.thunk:
+            func.setParentNamespace(namespace)
+            func = func.getThunkedFunction(False)
+
+        if not func.external:  # cant reparent external function
+            func.setParentNamespace(namespace)
 
     def get_vftable_entries(self, vftable):
+        """
+        Get a list of function pointer entries for a given vftable/vtable.
+        The returned list may also include Address(0) (NULL) entries,
+        as the vtable likely includes an uninitialized function poiner.
+        """
         vftable_addr = vftable.getAddress()
         if vftable.name.find(self._vtable_str) != -1:
             vftable_addr = vftable_addr.add(self.ptr_size*2)
@@ -109,7 +204,13 @@ class ClassMapper:
             addr = addr.add(self.ptr_size)
         return funcs
 
-    def get_datatype_of_thisptr(self, high_func):
+    def get_datatype_of_thisptr(self, func):
+        """
+        Get the datatype of a Function's `this` pointer.
+        """
+        if func.getCallingConvention().name != self._thiscall_str:
+            return None
+        high_func = self.get_high_function(func)
         prot = high_func.getFunctionPrototype()
         num_params = prot.getNumParams()
         if num_params == 0:
@@ -117,13 +218,60 @@ class ClassMapper:
         maybe_this = prot.getParam(0)
         return maybe_this.getDataType()
 
-# import class_mapper
-# cm = class_mapper.ClassMapper(currentProgram)
-# cm.associate_vftable_functions_with_namespace()
+    def _is_class_namespace_virtual(self, namespace):
+        """
+        Try to guess if a class namespace is virtual by looking for
+        'pure_virtual' in the function names associated with its vtable
+        """
+        funcs = self.namespace_functions.get(namespace, [])
+        for func in funcs:
+            if func == self._null:
+                continue
+
+            if func.name.find(self._pure_virtual_str) != -1:
+                return True
+
+        return False
+
+    def _find_private_function_of_class_namespace(self, namespace):
+        """
+        Check each function that is called by the vtable functions of
+        the given namespace to see if it is only called by functions
+        in that vtable or their decendant functions.
+
+        TODO: The graph traversal portion of this function is likely
+        better implemented in java, so decendant call analysis is not
+        implemented yet. Instead it is just a single call down for now.
+
+        TODO: May need to check references to the called function as well
+
+        TODO: handle thunks
+        """
+        namespace_functions = self.namespace_functions.get(namespace, set())
+        found_private_functions = set()
+        for func in namespace_functions:
+            if func == self._null:
+                continue
+            called_functions = func.getCalledFunctions(self._monitor)
+            for called_func in called_functions:
+                calling_functions = set(called_func.getCallingFunctions(self._monitor))
+                if calling_functions.issubset(namespace_functions):
+                    found_private_functions.add(called_func)
+
+        return found_private_functions
+
+
+# from class_mapper import ClassMapper
+# cm = ClassMapper(currentProgram)
+# cm.set_function_associations()
+
+if __name__ == '__main__':
+    cm = ClassMapper(currentProgram)
+    cm.set_function_associations()
+    print("Done Running!")
 
 # funcs = cm.get_vftable_entries(cm.class_syms[u'ActiveLoggerImpl'][2])
 # func = funcs[0]
-# high_func = cm.get_high_function(func)
-# datatype = cm.get_datatype_of_thisptr(high_func)
+# datatype = cm.get_datatype_of_thisptr(func)
 # base_datatype_name = datatype.displayName.replace(' *', '')
 # [b] = [i for i in cm.dtm.getAllStructures() if i.getName() == base_datatype_name]
