2f66eec93d1413b0f7d0312ff99455f066f2fa4d
Clif Wolfe <clifwolfe1@gmail.com>
2023-02-13 22:30:30 -0500

    updated description

--- name_functions_from_called_function.py
+++ name_functions_from_called_function.py
@@ -1,8 +1,16 @@
 # Auto-rename functions across a file based on the string passed to a specific function. 
 # It should be noted that the script only works for functions whose names start with `FUN_`, 
 # to avoid overwriting user-named functions.
+# 
 # It should also be noted that the script will only work if the parameter type has been 
-# set correctly in the target function's signature. E.g. change `undefined8` to `char *`
+# set correctly in the target function's signature. E.g. change `undefined8` to `char *`.
+# 
+# The script Is meant to be a quick and easy solution, and it does not actually emulate or 
+# interpret pcode in a meaningful way, it just tracks writes to register and stack locations
+# and relies on the assumption that in c and c++ a given space on the stack should only ever 
+# be utilized for a single type E.g. a pointer on the stack that is used for a `char *` 
+# should not ever be used to hold a `uint` unless there is a union containing the two types.
+# Keeping that in mind, the script can and will rename things incorrectly
 #@author Clifton Wolfe
 #@category C++
 
