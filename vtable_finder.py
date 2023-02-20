64e75772dd52305a4eb8b2970aa09874d3965e4f
Clif Wolfe <clifwolfe1@gmail.com>
2023-02-19 20:14:29 -0500

    use better creation function

--- vtable_finder.py
+++ vtable_finder.py
@@ -13,6 +13,10 @@ from ghidra.app.plugin.core.navigation.locationreferences import ReferenceUtils
 from ghidra.program.util import FunctionSignatureFieldLocation
 from ghidra.program.model.symbol import SourceType
 from ghidra.program.model.data import StructureDataType, PointerDataType, FunctionDefinitionDataType
+from ghidra.program.model.data import StructureFactory
+from ghidra.framework.plugintool import PluginTool
+from ghidra.app.cmd.data import CreateStructureCmd
+from ghidra.app.plugin.core.data import DataPlugin
 from collections import namedtuple
 import string
 import re
@@ -206,6 +210,10 @@ vtf = VTableFinder(currentProgram)
 found_vtables = vtf.find_vtables()
 # pointer_runs = vtf.find_pointer_runs(additional_search_block_filter=lambda a: a.name == ".rdata")
 
+# plugintool = PluginTool()
+# plugin = DataPlugin()
+# tool = plugin.getTool()
+struct_fact = StructureFactory()
 newname = "vtable"
 for found_vtable in found_vtables:
     location_sym = getSymbolAt(found_vtable.address)
@@ -215,7 +223,12 @@ for found_vtable in found_vtables:
     if re.search("vb?f?table", current_name_str) is None:
         location_sym.setName(newname, SourceType.USER_DEFINED)
 
-    new_struct = StructureDataType("vtable_%s" % str(found_vtable.address), found_vtable.size*vtf.ptr_size, vtf.dtm)
+    structure_name = "blah_%s" % str(found_vtable.address)
+    # new_struct = StructureDataType("vtable_%s" % str(found_vtable.address), found_vtable.size*vtf.ptr_size, vtf.dtm)
+    # this function uses the existing types of the data
+    new_struct = struct_fact.createStructureDataType(currentProgram, found_vtable.address, vtf.ptr_size*found_vtable.size, structure_name, True)
+    # cmd = CreateStructureCmd(new_struct, found_vtable.address)
+    # tool.execute(cmd, currentProgram)
     for i in range(found_vtable.size*vtf.ptr_size, vtf.ptr_size):
         datatype = PointerDataType()
         new_struct.replace(i, datatype, vtf.ptr_size)
