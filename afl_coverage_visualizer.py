1ff2ecc09ddf2781ae9dd3cf68a68e3ee9fbf376
Clif Wolfe <clifwolfe1@gmail.com>
2023-06-25 14:11:46 -0400

    much better coverage visualization showing flows of unconditional blocks that are known to have been reached

--- afl_coverage_visualizer.py
+++ afl_coverage_visualizer.py
@@ -1,12 +1,16 @@
-# Try to visualize the coverage from an afl++ bitmap in ghidra
-# generate a showmap file with
+# Visualize the coverage from an afl++ bitmap in ghidra.
+# Generate a showmap file with
 # `afl-showmap -C -i <afl-out-dir> -o showmap -- <fuzzed-binary>`
+# NOTE: it is possible for certain blocks at the start of your binary to be
+# missed if afl is running in persistent mode, so a red block at the start of
+# main is likely to be valida
 #@author Clifton Wolfe
 
 import ghidra
 from ghidra.program.model.block import BasicBlockModel
 from ghidra.program.model.address import AddressSet
-from collections import namedtuple
+from ghidra.program.model.symbol import FlowType
+from collections import namedtuple, defaultdict
 import re
 
 from java.awt import Color
@@ -23,7 +27,7 @@ SanCovPCGuardRef = namedtuple("SanCovPCGuardRef", ["ref",
 
 SANCOV_PC_GUARD_VALUE_SIZE = 4
 # this might change in different versions or with different builds,
-# but so far it seems to start at 6
+# but so far it seems to start here
 SANCOV_PC_GUARD_START_INDEX = 6
 COLOR_DEFAULT = Color(255, 255, 255)  # white
 COLOR_VISITED = Color(137, 207, 240)  # light blue
@@ -78,35 +82,38 @@ def get_pc_guard_refs():
     return pc_guard_refs
 
 
-class BasicBlockHighlighter:
+def get_code_block(address):
+    func = getFunctionContaining(address)
+    block_model = BasicBlockModel(currentProgram)
+    addresses = func.getBody()
+    code_blocks = list(block_model.getCodeBlocksContaining(addresses,
+                                                           monitor))
+    for code_block in code_blocks:
+        if code_block.contains(address):
+            return code_block
+    return None
+
+
+class BlockHighlighter:
     """
     Class for hightlighting basic blocks
     """
     def __init__(self):
-        self.hightlighted_block_record = set()
+        self.hightlighted_block_record = []
         self.listing = currentProgram.getListing()
 
-    def get_basic_block(self, address):
-        # cu = self.listing.getCodeUnitContaining(address)
-        func = getFunctionContaining(address)
-        block_model = BasicBlockModel(currentProgram)
-        addresses = func.getBody()
-        code_blocks = list(block_model.getCodeBlocksContaining(addresses,
-                                                               monitor))
-        for code_block in code_blocks:
-            if code_block.contains(address):
-                return code_block
-        return None
+    def highlight_code_block_of_address(self, address,
+                                        color=COLOR_VISITED):
+        bb = get_code_block(address)
+        self.highlight_code_block(bb)
 
-    def highlight_basic_block_of_address(self, address,
-                                         color=COLOR_VISITED):
-        bb = self.get_basic_block(address)
-        for address_range in bb.addressRanges:
+    def highlight_code_block(self, code_block, color=COLOR_VISITED):
+        for address_range in code_block.addressRanges:
             address_set = AddressSet(address_range)
             setBackgroundColor(address_set, color)
         # save record of this highlight so it can be
         # undone later
-        self.hightlighted_block_record.add(address)
+        self.hightlighted_block_record.append(code_block)
 
 
 def parse_indices_from_showmap_output(filepath):
@@ -134,7 +141,84 @@ def pc_guard_offset_to_showmap_index(offset):
     return (offset // SANCOV_PC_GUARD_VALUE_SIZE) + SANCOV_PC_GUARD_START_INDEX
 
 
-def hightlight_sancov_visited_basic_blocks():
+def get_code_block_sources(code_block):
+    """
+    block.SimpleSourceReferenceIterator is not iterable
+    """
+    sources = []
+    source_iterator = code_block.getSources(monitor)
+    while source_iterator.hasNext():
+        sources.append(source_iterator.next())
+    return sources
+
+
+def get_code_block_dests(code_block):
+    """
+    block.SimpleSourceReferenceIterator is not iterable
+    """
+    dests = []
+    dest_iterator = code_block.getDestinations(monitor)
+    while dest_iterator.hasNext():
+        dests.append(dest_iterator.next())
+    return dests
+
+
+class BlockFlowTracer:
+    def __init__(self, visited_blocks):
+        self._traced_blocks = set()
+        self._visited_blocks = set(visited_blocks)
+
+    def find_unconditional_source_blocks(self, code_block):
+        curr_code_block = code_block
+        unconditional_source_blocks = set()
+        self._visited_blocks.add(code_block)
+        while True:
+            block_sources = get_code_block_sources(curr_code_block)
+            if len(block_sources) != 1 or curr_code_block in unconditional_source_blocks:
+                break
+            src = block_sources[0]
+            src_block = src.getSourceBlock()
+            unconditional_source_blocks.add(src_block)
+            curr_code_block = src_block
+        self._visited_blocks.update(unconditional_source_blocks)
+        return unconditional_source_blocks
+
+    def find_unconditional_dest_blocks(self, code_block):
+        curr_code_block = code_block
+        unconditional_dest_blocks = set()
+        self._visited_blocks.add(code_block)
+        while True:
+            block_dests = get_code_block_dests(curr_code_block)
+            if len(block_dests) != 1 or curr_code_block in unconditional_dest_blocks:
+                break
+            dest = block_dests[0]
+            dest_block = dest.getDestinationBlock()
+            unconditional_dest_blocks.add(dest_block)
+            curr_code_block = dest_block
+        self._visited_blocks.update(unconditional_dest_blocks)
+        return unconditional_dest_blocks
+
+    def get_unconditionally_visited_blocks(self, code_block):
+        all_visited_blocks = set()
+        usb = self.find_unconditional_source_blocks(code_block)
+        udb = self.find_unconditional_dest_blocks(code_block)
+        all_visited_blocks.update(usb)
+        all_visited_blocks.update(udb)
+        all_visited_blocks.add(code_block)
+        return all_visited_blocks
+
+    def get_all_unconditionally_visited_blocks(self):
+        # copy so that blocks don't get processed twice
+        # during this process, as visited_blocks gets updated
+        visited_blocks_copy = list(self._visited_blocks)
+        all_visited_blocks = set(visited_blocks_copy)
+        for visited_block in visited_blocks_copy:
+            uvb = self.get_unconditionally_visited_blocks(visited_block)
+            all_visited_blocks.update(uvb)
+        return all_visited_blocks
+
+
+def hightlight_sancov_visited_code_blocks():
     """
     Currently the main functionality of the script.
     """
@@ -145,20 +229,31 @@ def hightlight_sancov_visited_basic_blocks():
     showmap_indices = parse_indices_from_showmap_output(showmap_filepath)
     showmap_offsets = set([showmap_index_to_pc_guard_offset(i) for i in showmap_indices])
     pc_guard_refs = get_pc_guard_refs()
-    bbh = BasicBlockHighlighter()
-    unused_pc_guard_refs = []
+    bbh = BlockHighlighter()
+    unreached_code_blocks = []
+    all_visited_blocks = []
     # TODO: these can likely be optimized a bit
     for pc_guard_ref in pc_guard_refs:
-        addr = pc_guard_ref.ref.fromAddress
+        code_block = get_code_block(pc_guard_ref.ref.fromAddress)
+        # pick out the ones that are not visited
         if pc_guard_ref.offset not in showmap_offsets:
-            unused_pc_guard_refs.append(pc_guard_ref)
+            unreached_code_blocks.append(code_block)
             continue
-        bbh.highlight_basic_block_of_address(addr, COLOR_VISITED)
 
-    for pc_guard_ref in unused_pc_guard_refs:
-        addr = pc_guard_ref.ref.fromAddress
-        bbh.highlight_basic_block_of_address(addr, COLOR_UNVISITED)
+        all_visited_blocks.append(code_block)
+
+    all_blocks_to_highlight = all_visited_blocks
+    # Find blocks that must be unconditionally reached
+    bft = BlockFlowTracer(all_visited_blocks)
+    all_blocks_to_highlight = bft.get_all_unconditionally_visited_blocks()
+
+    for block in unreached_code_blocks:
+        bbh.highlight_code_block(block, COLOR_UNVISITED)
+
+    for block in all_blocks_to_highlight:
+        bbh.highlight_code_block(block, COLOR_VISITED)
+
 
 
 if __name__ == "__main__":
-    hightlight_sancov_visited_basic_blocks()
+    hightlight_sancov_visited_code_blocks()
