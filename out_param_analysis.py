76c2ede166225942c65981e44cf2b7271911ecc1
Clif Wolfe <clifwolfe1@gmail.com>
2025-07-16 11:28:37 -0400

    made an object for out param analysis results

--- out_param_analysis.py
+++ out_param_analysis.py
@@ -12,30 +12,41 @@ log.addHandler(logging.StreamHandler())
 log.setLevel(logging.WARNING)
 
 
+class OutParamAnalysisResult:
+    def __init__(self, func_name, args=None):
+        self.func_name = func_name
+        if args:
+            self.args = args
+        else:
+            self.args = []
+        # memcpy_s
+        self.stack_dest_var_dest_size_by_func = defaultdict(set)
+        # memcpy or strcpy
+        self.stack_dest_var_or_no_src_size_by_func = defaultdict(set)
+        # memcpy or strcpy, but could lead to an info leak
+        self.stack_src_var_or_no_src_size_by_func = defaultdict(set)
+        # all param: needs additional analysis
+        self.all_params_var_by_func = defaultdict(set)
+        # possible overflow in a global
+        self.const_dest_var_or_no_src_size_by_func = defaultdict(set)
+
+
 def out_param_analysis(func_name, out_param_no, src_size_param_no=None, dest_size_param_no=None, src_param_no=None, program=None):
     if program is None:
         program = currentProgram
     stack_reg_offset = getStackRegister().getOffset()
-    
+
     du = DecompUtils(program=program)
-    # memcpy_s
-    stack_dest_var_dest_size_by_func = defaultdict(set)
-    # memcpy or strcpy
-    stack_dest_var_or_no_src_size_by_func = defaultdict(set)
-    # memcpy or strcpy, but could lead to an info leak
-    stack_src_var_or_no_src_size_by_func = defaultdict(set)
-    # all param: needs additional analysis
-    all_params_var_by_func = defaultdict(set)
-    # possible overflow in a global
-    const_dest_var_or_no_src_size_by_func = defaultdict(set)
-    
+    args = [out_param_no, src_size_param_no, dest_size_param_no, src_param_no]
+    opar = OutParamAnalysisResult(func_name, args)
+
     callsites = get_callsites_for_func_by_name(func_name, program=program)
     for calling_func, call_addrs in callsites.items():
         pcode_ops = du.get_pcode_for_function(calling_func)
         call_ops = [i for i in pcode_ops if i.opcode == PcodeOpAST.CALL and i.seqnum.target in call_addrs]
         for op in call_ops:
             stack_dest = False
-            stack_src = False 
+            stack_src = False
             const_or_addr_dest = False
             # TODO: an addr src from a rw region is still useful if size is not const
             const_or_addr_src = False
@@ -79,27 +90,26 @@ def out_param_analysis(func_name, out_param_no, src_size_param_no=None, dest_siz
                     back_slice_vns = DecompilerUtils.getBackwardSlice(inp)
                     if any([vn for vn in back_slice_vns if vn.isRegister() and int(vn.getOffset()) == stack_reg_offset]):
                         stack_dest = True
-            # group this particular call based on gathered attributes            
+            # group this particular call based on gathered attributes
             # memcpy_s
             if stack_dest and var_dest_size:
-                stack_dest_var_dest_size_by_func[calling_func].add(op.seqnum.target)
+                opar.stack_dest_var_dest_size_by_func[calling_func].add(op.seqnum.target)
 
             # memcpy or strcpy
             if stack_dest and var_src_size:
-                stack_dest_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
+                opar.stack_dest_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
 
             # memcpy or strcpy, but could lead to an info leak
             if stack_src and var_src_size:
-                stack_src_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
+                opar.stack_src_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
 
             # all param: needs additional analysis
             if var_dest and var_src_size and (dest_size_param_no is None or var_dest_size is True) and var_src:
-                all_params_var_by_func[calling_func].add(op.seqnum.target)
+                opar.all_params_var_by_func[calling_func].add(op.seqnum.target)
 
             # possible overflow in a global
             if const_or_addr_dest and var_src_size:
-                const_dest_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
-                
-            
-    return (stack_dest_var_dest_size_by_func, stack_dest_var_or_no_src_size_by_func, stack_src_var_or_no_src_size_by_func, all_params_var_by_func, const_dest_var_or_no_src_size_by_func)
-    
+                opar.const_dest_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
+
+    return opar
+
