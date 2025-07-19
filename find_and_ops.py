67951af6023b5e690283aaefc388fe686819dba9
Clif Wolfe <clifwolfe1@gmail.com>
2025-07-19 09:23:04 -0400

    fixed script

--- find_and_ops.py
+++ find_and_ops.py
@@ -1,5 +1,5 @@
 from __main__ import *
-from ghidra.program.model import PcodeOpAST
+from ghidra.program.model.pcode import PcodeOpAST
 from ghidra.program.database.code import InstructionDB
 from ghidra.program.model.symbol import RefType, SourceType, MemReferenceImpl
 from collections import defaultdict
@@ -10,12 +10,12 @@ funcs_to_opaddrs = defaultdict(list)
 listing = currentProgram.getListing()
 instructions = listing.getInstructions(True)
 for instr in instructions:
-    for raw_ops in instr.getPcode():
-        for op in raw_ops:
-            if op.opcode == PcodeOpAST.INT_AND:
-                addr = op.seqnum.target
-                func = getFunctionContaining(addr)
-                funcs_to_opaddrs[func].append(addr)
+    raw_ops = list(instr.getPcode())
+    for op in raw_ops:
+        if op.opcode == PcodeOpAST.INT_AND:
+            addr = op.seqnum.target
+            func = getFunctionContaining(addr)
+            funcs_to_opaddrs[func].append(addr)
 
 for func, addrs in funcs_to_opaddrs.items():
     print(func)
