762a0fc00ef6be34de30b03cf10373528453c7ea
Clif Wolfe <clifwolfe1@gmail.com>
2025-07-20 13:26:28 -0400

    added missing loop

--- out_param_analysis.py
+++ out_param_analysis.py
@@ -66,76 +66,77 @@ def out_param_analysis(desc_col, program=None):
 
     stack_reg_offset = getStackRegister().getOffset()
     du = DecompUtils(program=program)
-    for calling_func, desc in desc_col.get_calling_func_to_desc_map().items():
-        call_addrs = desc.callsites[calling_func]
-        pcode_ops = du.get_pcode_for_function(calling_func)
-        call_ops = [i for i in pcode_ops if i.opcode == PcodeOpAST.CALL and i.seqnum.target in call_addrs]
-        for op in call_ops:
-            stack_dest = False
-            stack_src = False
-            const_or_addr_dest = False
-            # TODO: an addr src from a rw region is still useful if size is not const
-            const_or_addr_src = False
-            var_dest_size = False
-            # variable src size can be considered to be true if it isn't existent because that means that the size is inferred
-            var_src_size = False
-            if desc.src_size_param_no is None:
-                var_src_size = True
-            var_dest = False
-            var_src = False
-            if desc.src_param_no is None:
-                var_src = True
-            # gather attribute info about the call and the parameters to it that we care about
-            if desc.src_size_param_no is not None:
-                inp = op.getInput(desc.src_size_param_no)
-                if inp is not None:
-                    if inp.isConstant() is False:
-                        var_src_size = True
-            if desc.dest_size_param_no is not None:
-                inp = op.getInput(desc.dest_size_param_no)
-                if inp is not None:
-                    if inp.isConstant() is False:
-                        var_dest_size = True
-            if desc.src_param_no is not None:
-                inp = op.getInput(desc.src_param_no)
-                if inp is not None:
-                    if inp.isConstant() is True or inp.isAddress() is True:
-                        const_or_addr_src = True
-                    else:
-                        var_src = True
-                    back_slice_vns = DecompilerUtils.getBackwardSlice(inp)
-                    if any([vn for vn in back_slice_vns if vn.isRegister() and int(vn.getOffset()) == stack_reg_offset]):
-                        stack_src = True
-            if desc.out_param_no is not None:
-                inp = op.getInput(desc.out_param_no)
-                if inp is not None:
-                    if inp.isConstant() is True or inp.isAddress() is True:
-                        const_or_addr_dest = True
-                    else:
-                        var_dest = True
-                    back_slice_vns = DecompilerUtils.getBackwardSlice(inp)
-                    if any([vn for vn in back_slice_vns if vn.isRegister() and int(vn.getOffset()) == stack_reg_offset]):
-                        stack_dest = True
-            # group this particular call based on gathered attributes
-            # memcpy_s
-            if stack_dest and var_dest_size:
-                desc.stack_dest_var_dest_size_by_func[calling_func].add(op.seqnum.target)
+    for calling_func, descs in desc_col.get_calling_func_to_desc_map().items():
+        for desc in descs:
+            call_addrs = desc.callsites[calling_func]
+            pcode_ops = du.get_pcode_for_function(calling_func)
+            call_ops = [i for i in pcode_ops if i.opcode == PcodeOpAST.CALL and i.seqnum.target in call_addrs]
+            for op in call_ops:
+                stack_dest = False
+                stack_src = False
+                const_or_addr_dest = False
+                # TODO: an addr src from a rw region is still useful if size is not const
+                const_or_addr_src = False
+                var_dest_size = False
+                # variable src size can be considered to be true if it isn't existent because that means that the size is inferred
+                var_src_size = False
+                if desc.src_size_param_no is None:
+                    var_src_size = True
+                var_dest = False
+                var_src = False
+                if desc.src_param_no is None:
+                    var_src = True
+                # gather attribute info about the call and the parameters to it that we care about
+                if desc.src_size_param_no is not None:
+                    inp = op.getInput(desc.src_size_param_no)
+                    if inp is not None:
+                        if inp.isConstant() is False:
+                            var_src_size = True
+                if desc.dest_size_param_no is not None:
+                    inp = op.getInput(desc.dest_size_param_no)
+                    if inp is not None:
+                        if inp.isConstant() is False:
+                            var_dest_size = True
+                if desc.src_param_no is not None:
+                    inp = op.getInput(desc.src_param_no)
+                    if inp is not None:
+                        if inp.isConstant() is True or inp.isAddress() is True:
+                            const_or_addr_src = True
+                        else:
+                            var_src = True
+                        back_slice_vns = DecompilerUtils.getBackwardSlice(inp)
+                        if any([vn for vn in back_slice_vns if vn.isRegister() and int(vn.getOffset()) == stack_reg_offset]):
+                            stack_src = True
+                if desc.out_param_no is not None:
+                    inp = op.getInput(desc.out_param_no)
+                    if inp is not None:
+                        if inp.isConstant() is True or inp.isAddress() is True:
+                            const_or_addr_dest = True
+                        else:
+                            var_dest = True
+                        back_slice_vns = DecompilerUtils.getBackwardSlice(inp)
+                        if any([vn for vn in back_slice_vns if vn.isRegister() and int(vn.getOffset()) == stack_reg_offset]):
+                            stack_dest = True
+                # group this particular call based on gathered attributes
+                # memcpy_s
+                if stack_dest and var_dest_size:
+                    desc.stack_dest_var_dest_size_by_func[calling_func].add(op.seqnum.target)
 
-            # memcpy or strcpy
-            if stack_dest and var_src_size:
-                desc.stack_dest_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
+                # memcpy or strcpy
+                if stack_dest and var_src_size:
+                    desc.stack_dest_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
 
-            # memcpy or strcpy, but could lead to an info leak
-            if stack_src and var_src_size:
-                desc.stack_src_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
+                # memcpy or strcpy, but could lead to an info leak
+                if stack_src and var_src_size:
+                    desc.stack_src_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
 
-            # all param: needs additional analysis
-            if var_dest and var_src_size and (desc.dest_size_param_no is None or var_dest_size is True) and var_src:
-                desc.all_params_var_by_func[calling_func].add(op.seqnum.target)
+                # all param: needs additional analysis
+                if var_dest and var_src_size and (desc.dest_size_param_no is None or var_dest_size is True) and var_src:
+                    desc.all_params_var_by_func[calling_func].add(op.seqnum.target)
 
-            # possible overflow in a global
-            if const_or_addr_dest and var_src_size:
-                desc.const_dest_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
+                # possible overflow in a global
+                if const_or_addr_dest and var_src_size:
+                    desc.const_dest_var_or_no_src_size_by_func[calling_func].add(op.seqnum.target)
 
     return desc_col
 
